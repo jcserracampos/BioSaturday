c94b851b03802b64b5ecdafcfa9e3a2ad93ffbb8
Rafael Veber de Carvalho <rafaelvebercarvalho@gmail.com>
2020-08-08 11:23:45 -0300

    create first contact

--- lib/bio_saturday.rb
+++ lib/bio_saturday.rb
@@ -3,3 +3,14 @@ require 'sqlite3'
 db = SQLite3::Database.new 'agenda.db'
 
 db.execute 'CREATE TABLE IF NOT EXISTS Contatos(nome TEXT, numero TEXT)'
+
+Event = Struct.new(:nome, :numero)
+contatos = []
+contatos << Event.new('Rafael Veber de Carvalho', '998216244')
+contatos << Event.new('Matheus Damasceno', '981265849')
+contatos << Event.new('Julio Campos', '9963176727')
+contatos.sort_by { |event| [event.numero, event.nome] }
+
+contatos.each do |contato|
+    db.execute "INSERT INTO Contatos VALUES ('#{contato.nome}', '#{contato.numero}');"
+end
