b8b458cfcafd2068d8943dcfafd1de0532f995f0
mdamasceno <matheus.id@hotmail.com>
2020-08-08 12:15:33 -0300

    update contact input

--- lib/bio_saturday.rb
+++ lib/bio_saturday.rb
@@ -4,13 +4,19 @@ db = SQLite3::Database.new 'agenda.db'
 
 db.execute 'CREATE TABLE IF NOT EXISTS Contatos(nome TEXT, numero TEXT)'
 
-Event = Struct.new(:nome, :numero)
-contatos = []
-contatos << Event.new('Rafael Veber de Carvalho', '998216244')
-contatos << Event.new('Matheus Damasceno', '981265849')
-contatos << Event.new('Julio Campos', '9963176727')
-contatos.sort_by { |event| [event.numero, event.nome] }
 
-contatos.each do |contato|
-    db.execute "INSERT INTO Contatos VALUES ('#{contato.nome}', '#{contato.numero}');"
-end
+
+
+puts 'Digite seu nome'
+nome = gets.chomp
+
+puts "Olá #{nome}, digite seu numero de telefone"
+numero = gets.chomp
+
+
+Contacts = Struct.new(:nome, :numero)
+contatos = []
+contatos << Contacts.new(nome, numero)
+contatos.sort_by { |contact| [contact.numero, contact.nome] }
+db.execute "INSERT INTO Contatos VALUES ('#{contatos[0].nome}', '#{contatos[0].numero}');"
+
