0992105ea953f77c97e27a3120f25a8b66d0bfa5
mdamasceno <matheus.id@hotmail.com>
2020-08-11 15:40:30 -0300

    update enterprise contact

--- lib/bio_saturday.rb
+++ lib/bio_saturday.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 
 db = SQLite3::Database.new 'agenda.db'
 
-db.execute 'CREATE TABLE IF NOT EXISTS Contatos(nome TEXT, numero TEXT)'
+db.execute 'CREATE TABLE IF NOT EXISTS Contatos(nome TEXT, numero TEXT, empresa TEXT)'
 
 
 
@@ -13,10 +13,16 @@ nome = gets.chomp
 puts "Olá #{nome}, digite seu numero de telefone"
 numero = gets.chomp
 
+puts "Para qual empresa trabalha?"
+empresa = gets.chomp
 
-Contacts = Struct.new(:nome, :numero)
+
+Contacts = Struct.new(:nome, :numero, :empresa)
 contatos = []
-contatos << Contacts.new(nome, numero)
-contatos.sort_by { |contact| [contact.numero, contact.nome] }
-db.execute "INSERT INTO Contatos VALUES ('#{contatos[0].nome}', '#{contatos[0].numero}');"
+contatos << Contacts.new(nome, numero, empresa)
+contatos.sort_by { |contact| [contact.numero, contact.nome, contact.empresa] }
 
+db.execute "INSERT INTO Contatos VALUES ('#{contatos[0].nome}', '#{contatos[0].numero}', '#{contatos[0].empresa}');"
+
+
+ 
