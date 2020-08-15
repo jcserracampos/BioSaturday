27e3f2c2b17e228aca50a2afff0069b0cff9edcc
mdamasceno <matheus.id@hotmail.com>
2020-08-15 12:11:13 -0300

    CriaMetodosContato

--- lib/bio_saturday.rb
+++ lib/bio_saturday.rb
@@ -1,11 +1,19 @@
+# Invocando as dependências
 require 'sqlite3'
 
-db = SQLite3::Database.new 'agenda.db'
+# Criando o banco de dados e a tabela
+@db = SQLite3::Database.new 'agenda.db'
 
-db.execute 'CREATE TABLE IF NOT EXISTS Contatos(nome TEXT, numero TEXT, empresa TEXT)'
+@db.execute 'CREATE TABLE IF NOT EXISTS Contatos(nome TEXT, numero TEXT, empresa TEXT)'
 
+Contacts = Struct.new(:nome, :numero, :empresa)
 
-
+# Declarar as funções
+def insert_contact(name, phone, company)
+    contato = Contacts.new(name, phone, company)
+    
+    @db.execute "INSERT INTO Contatos VALUES ('#{contato.nome}', '#{contato.numero}', '#{contato.empresa}');"
+end
 
 puts 'Digite seu nome'
 nome = gets.chomp
@@ -16,13 +24,4 @@ numero = gets.chomp
 puts "Para qual empresa trabalha?"
 empresa = gets.chomp
 
-
-Contacts = Struct.new(:nome, :numero, :empresa)
-contatos = []
-contatos << Contacts.new(nome, numero, empresa)
-contatos.sort_by { |contact| [contact.numero, contact.nome, contact.empresa] }
-
-db.execute "INSERT INTO Contatos VALUES ('#{contatos[0].nome}', '#{contatos[0].numero}', '#{contatos[0].empresa}');"
-
-
- 
+insert_contact(nome, numero, empresa)
